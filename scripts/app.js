cf3f6e4a86b77617cb905381775876e391f70946
jason-toups <me@jasontoups.com>
2020-01-13 23:10:22 -0800

    added hours ago to the DOM, and got the magnitude indicator to work

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
-// var image = '/images/earthquake.png';
 
 var image = {
   url: '/images/earthquake.png', // url
@@ -13,16 +12,26 @@ $(document).ready(function () {
   console.log("Let's get coding!");
   // CODE IN HERE!
 });
+
 const onSuccess = response => {
   initMap();
   const { features } = response;
   features.forEach(earthquake => {
     console.log(earthquake);
-    const timeAgo = timeDiff(earthquake.properties.time);
-    console.log(timeAgo);
+    const hoursAgo = timeDiff(earthquake.properties.time);
+    const mag = earthquake.properties.mag;
+    let magDot = '';
+    if (mag >= 4 && mag < 5) {
+      magDot = '<span class=yellow>O</span>';
+    } else if (mag >= 5 && mag < 6) {
+      magDot = '<span class=orange>O</span>';
+    } else if (mag >= 6) {
+      magDot = '<span class=red>O</span>';
+    }
+
 
     const quakeName = earthquake.properties.title;
-    const template = `<p>${quakeName}</p>`;
+    const template = `<p>${magDot} ${quakeName}</span>, ${hoursAgo} hours ago</p>`;
     $('#info').append(template);
 
     const coords = earthquake.geometry.coordinates;
@@ -38,18 +47,19 @@ const onSuccess = response => {
 const onError = (error, errorText, errorCode) => {
   console.log({ error })
 };
+
 $.ajax({
   method: 'GET',
   url: weekly_quakes_endpoint,
   success: onSuccess,
   error: onError
 })
+
 const timeDiff = (then) => {
   let now = Date.now();
   let hrsAgo = (((now - then) / 1000) / 60) / 60;
   return Math.round(hrsAgo);
 };
-
 
 var map;
 function initMap() {
@@ -58,17 +68,4 @@ function initMap() {
     center: sanFrancisco,
     zoom: 2
   });
-  // var marker = new google.maps.Marker({ position: sanFrancisco, map: map });
-  // Create a <script> tag and set the USGS URL as the source.
-  // var script = `<script src='https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js'></script>`;
-  // // This example uses a local copy of the GeoJSON stored at
-  // // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-  // $('#map').prepend(script);
 }
-
-
-
-
-
-// initMap();
-// $('#map').append(map);
