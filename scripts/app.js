0242dd2b28682918863ae422ff9aacb587046139
jason-toups <me@jasontoups.com>
2020-01-16 13:33:49 -0800

    adds colored markers to map

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,23 @@
 // define globals
-var weekly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+let weekly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 
-var monthly_quakes_endpoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+let monthly_quakes_endpoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
 
-var quakes_endpoint = [weekly_quakes_endpoint, monthly_quakes_endpoint];
+let quakes_endpoint = [weekly_quakes_endpoint, monthly_quakes_endpoint];
 
-var sfLatLng = (37.7749, 122.4194);
+let sfLatLng = (37.7749, 122.4194);
 
-var image = {
+let image = {
   url: 'images/earthquakeIcon-red.svg', // url
   scaledSize: new google.maps.Size(26, 26), // scaled size
   origin: new google.maps.Point(0, 0), // origin
   anchor: new google.maps.Point(0, 13) // anchor
 };
+
+// let markers = {
+//   color: 'red',
+//   url = 'images/earthquakeIcon-red.svg'
+// }
 
 $(document).ready(function () {
   console.log("Let's get coding!");
@@ -27,12 +32,22 @@ const onSuccess = response => {
     const hoursAgo = timeDiff(earthquake.properties.time);
     const mag = earthquake.properties.mag;
     let magDot = '';
-    if (mag >= 4 && mag < 5) {
+
+    if (mag >= 4 && mag < 4.5) {
       magDot = '<span class=yellow>O</span>';
-    } else if (mag >= 5 && mag < 6) {
+      image.url = 'images/earthquakeIcon-purple.svg'
+    } else if (mag >= 4.5 && mag < 5) {
+      magDot = '<span class=yellow>O</span>';
+      image.url = 'images/earthquakeIcon-blue.svg'
+    } else if (mag >= 5 && mag < 5.5) {
       magDot = '<span class=orange>O</span>';
+      image.url = 'images/earthquakeIcon-yellow.svg'
+    } else if (mag >= 5.5 && mag < 6) {
+      magDot = '<span class=orange>O</span>';
+      image.url = 'images/earthquakeIcon-orange.svg'
     } else if (mag >= 6) {
       magDot = '<span class=red>O</span>';
+      image.url = 'images/earthquakeIcon-red.svg'
     }
 
 
