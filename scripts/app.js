880f1985bbc249f04c7b74f6f786fcf275cb4dc3
jason-toups <me@jasontoups.com>
2020-01-16 21:18:18 -0800

    made markers bigger, added quake object, calculated max and min magnitude values

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 // define globals
-let weekly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+let weekly_quakes_endpoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
 
-let monthly_quakes_endpoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson'
+let monthly_quakes_endpoint = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
 let quakes_endpoint = [weekly_quakes_endpoint, monthly_quakes_endpoint];
 
@@ -9,29 +9,49 @@ let sfLatLng = (37.7749, 122.4194);
 
 let image = {
   url: 'images/earthquakeIcon-red.svg', // url
-  scaledSize: new google.maps.Size(26, 26), // scaled size
+  scaledSize: new google.maps.Size(36, 36), // scaled size
   origin: new google.maps.Point(0, 0), // origin
-  anchor: new google.maps.Point(0, 13) // anchor
+  anchor: new google.maps.Point(0, 18) // anchor
 };
 
-// let markers = {
-//   color: 'red',
-//   url = 'images/earthquakeIcon-red.svg'
-// }
+const quakes = {
+  magMin: 6,
+  magMax: 0,
+  weeklyQuakesEndpoint: "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson",
+  monthlyQuakesEndpoint: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson',
+  sfLatLng: (37.7749, 122.4194),
+  image: {
+    url: 'images/earthquakeIcon-red.svg', // url
+    scaledSize: new google.maps.Size(200, 200), // scaled size
+    origin: new google.maps.Point(0, 0), // origin
+    anchor: new google.maps.Point(0, 100) // anchor
+  },
+  response: [],
+};
+
 
 $(document).ready(function () {
-  console.log("Let's get coding!");
+  // console.log("Let's get coding!");
   // CODE IN HERE!
 });
 
+/* ORIGINAL - This works, somewhat */
 const onSuccess = response => {
   initMap();
+  quakes.response = response;
   const { features } = response;
   features.forEach(earthquake => {
-    console.log(earthquake);
+    // console.log(earthquake);
     const hoursAgo = timeDiff(earthquake.properties.time);
     const mag = earthquake.properties.mag;
     let magDot = '';
+
+    if (mag < quakes.magMin) {
+      quakes.magMin = mag;
+    }
+    if (mag > quakes.magMax) {
+      quakes.magMax = mag;
+    }
 
     if (mag >= 4 && mag < 4.5) {
       magDot = '<span class=yellow>O</span>';
@@ -50,26 +70,100 @@ const onSuccess = response => {
       image.url = 'images/earthquakeIcon-red.svg'
     }
 
+    const place = earthquake.properties.place;
+    /* This doesn't entirely work, can't figure out how to split the titles to account for all the inconsistent formatting in the feed */
+    // let quakeName = title.split(" ");
+    // let quakeNameSlice = quakeName.slice(3, quakeName.length).join(" ");
 
-    const title = earthquake.properties.title;
-    let quakeName = title.split("of ")[1];
+    // if (quakeNameSlice.length === 0) {
+    //   quakeNameSlice = title;
+    // }
 
-    const template = `<p>${magDot}magnitude ${mag} ${quakeName}</span>, ${hoursAgo} hours ago</p>`;
+    const template = `<p>${mag} ${magDot} ${place}</span>, ${hoursAgo} hours ago</p>`;
     $('#info').append(template);
 
     const coords = earthquake.geometry.coordinates;
-    console.log(coords);
+    // console.log(coords);
     const latLng = new google.maps.LatLng(coords[1], coords[0]);
     const marker = new google.maps.Marker({
       position: latLng,
       map: map,
-      icon: image
+      icon: image,
     });
   });
 };
 const onError = (error, errorText, errorCode) => {
   console.log({ error })
 };
+
+/*  Split this into a few functions */
+// First I need to find the max and min numbers for the magnitude
+// Then I need to loop through the quakes.response, and place the markers on the map
+// I need to rewrite the markers check, to put different colors on the markers.
+
+// const onSuccess = response => {
+//   initMap();
+//   quakes.response = response;
+// };
+
+// // const { features } = quakes.response;
+// quakes.response.features.forEach(earthquake => {
+//   // console.log(earthquake);
+//   const hoursAgo = timeDiff(earthquake.properties.time);
+//   const mag = earthquake.properties.mag;
+//   let magDot = '';
+
+//   if (mag < quakes.magMin) {
+//     quakes.magMin = mag;
+//   }
+//   if (mag > quakes.magMax) {
+//     quakes.magMax = mag;
+//   }
+
+//   if (mag >= 4 && mag < 4.5) {
+//     magDot = '<span class=yellow>O</span>';
+//     image.url = 'images/earthquakeIcon-purple.svg'
+//   } else if (mag >= 4.5 && mag < 5) {
+//     magDot = '<span class=yellow>O</span>';
+//     image.url = 'images/earthquakeIcon-blue.svg'
+//   } else if (mag >= 5 && mag < 5.5) {
+//     magDot = '<span class=orange>O</span>';
+//     image.url = 'images/earthquakeIcon-yellow.svg'
+//   } else if (mag >= 5.5 && mag < 6) {
+//     magDot = '<span class=orange>O</span>';
+//     image.url = 'images/earthquakeIcon-orange.svg'
+//   } else if (mag >= 6) {
+//     magDot = '<span class=red>O</span>';
+//     image.url = 'images/earthquakeIcon-red.svg'
+//   }
+
+//   const place = earthquake.properties.place;
+//   /* This doesn't entirely work, can't figure out how to split the titles to account for all the inconsistent formatting in the feed */
+//   // let quakeName = title.split(" ");
+//   // let quakeNameSlice = quakeName.slice(3, quakeName.length).join(" ");
+
+//   // if (quakeNameSlice.length === 0) {
+//   //   quakeNameSlice = title;
+//   // }
+
+//   const template = `<p>${mag} ${magDot} ${place}</span>, ${hoursAgo} hours ago</p>`;
+//   $('#info').append(template);
+
+//   const coords = earthquake.geometry.coordinates;
+//   // console.log(coords);
+//   const latLng = new google.maps.LatLng(coords[1], coords[0]);
+//   const marker = new google.maps.Marker({
+//     position: latLng,
+//     map: map,
+//     icon: image,
+//   });
+// });
+
+// const onError = (error, errorText, errorCode) => {
+//   console.log({ error })
+// };
+
+/* -------End of refactor -------- */
 
 $.ajax({
   method: 'GET',
@@ -91,6 +185,11 @@ function initMap() {
     center: sanFrancisco,
     zoom: 2
   });
+  marker = new google.maps.Marker({
+    position: sanFrancisco,
+    map: map,
+  });
+
 }
 
 /* makeButtons works, but the buttons aren't centered in the row, and the buttons aren't hooked up to an event listener */
